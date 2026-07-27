aa8f8447ece1c145c300af7ba5c4f564aefbbe9f
Eric Karkovack <webmaster@karks.com>
2026-07-27 07:59:48 -0400

    Version Bump

--- karks-crm.php
+++ karks-crm.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Karks CRM
  * Description: Manage customers, services, and invoices across multiple companies.
- * Version: 0.6.1
+ * Version: 0.6.2
  * Author: Eric Karkovack
  * Author URI: https://karks.com
  * Text Domain: karks-crm
